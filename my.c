a10d73b75d8a6d3a2369ef60c97023515922e116
Syed M.Jafar Ali Naqvi <117013434+laibashkh@users.noreply.github.com>
2022-11-01 12:49:29 +0500

    Code merged
    Code of both partners merged together.

--- my.c
+++ my.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int array[100] = { 0 };
+	int array[100] = {0};
 	int option, class_type, seat_number;
 	printf("\nChoose an option\n");
 	printf("\n1.Seat availability chart\n2.Seat booking\n3.Exit\n\nEnter here: ");
@@ -19,7 +19,49 @@ int main()
 					printf("\n");
 					printf("\t\t\t");
 				}
+				
 			}
 		}
+if (option == 2)
+		{
+			printf("\n\tType 1 for first class\n\tType 2 for economy class");
+			printf("\n\n\tSelect type of class: ");
+			scanf_s("%d", &class_type);
+			if (class_type == 1)
+			{
+				printf("\n\tSelect seat number (1-30): ");
+				scanf_s("%d", &seat_number);
+				if (seat_number > 0 && seat_number <= 30)
+				{
+					array[seat_number-1] = 1;
+					printf("\t\t_______________________________________________________________________________\n");
+					printf("\t\t\t\t\tBoarding pass\n\t\t\t\tclass:\t\t\t\tFirst class\n\t\t\t\tSeat number: \t\t\t%d\n",seat_number);
+					printf("\t\t_______________________________________________________________________________\n");
+				}
+				else
+				{
+					printf("\n\t\tThis seat is not available Enter any other number!\n");
+				}
+			}
+			if (class_type == 2)
+			{
+				printf("\tSelect seat number (31-100): ");
+				scanf_s("%d", &seat_number);
+				if (seat_number > 30 && seat_number <= 100)
+				{
+					array[seat_number-1] = 1;
+					printf("\t\t_______________________________________________________________________________\n");
+					printf("\t\t\t\t\tBoarding pass\n\t\t\t\tclass:\t\t\t\tEconomy class\n\t\t\t\tSeat number: \t\t\t%d\n", seat_number);
+					printf("\t\t_______________________________________________________________________________\n");
+				}
+				else
+				{
+					printf("\n\t\tThis seat is not available Enter any other number!\n");
+				}
+			}
+		}
+		printf("\nChoose an option\n");
+		printf("1.Seat availability chart\n2.Seat booking\n3.Exit\n\nEnter here: ");
+		scanf("%d",&option);
 	}
-}
+}	
